c477974f979551b303d31468b5d56ed15d957d17
loongmxbt <loongmxbt@gmail.com>
2014-12-14 22:20:06 +0800

    add module node

---
+++ modules/node/node.go
@@ -0,0 +1,33 @@
+package node
+
+import (
+	"github.com/Unknwon/macaron"
+)
+
+func GetNodesAll(ctx *macaron.Context) {
+
+}
+
+func GetNodesByTag(ctx *macaron.Context) {
+
+}
+
+func GetNodesByType(ctx *macaron.Context) {
+
+}
+
+func GetNodeById(ctx *macaron.Context) {
+
+}
+
+func GetNodeBySlug(ctx *macaron.Context) {
+
+}
+
+func GetNode(ctx *macaron.Context) {
+
+}
+
+func CreateNode(ctx *macaron.Context) {
+
+}
